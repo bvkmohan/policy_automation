a62f7998da552dd1658db4df672843f8f7d9cb12
Mohan Bala Venkata Krishna <mobkrish@in.ibm.com>
2019-08-11 15:59:41 +0530

    Added code to detect disabled/deactivated interfaces

--- SRX.py
+++ SRX.py
@@ -13,6 +13,7 @@ class SRXConfig:
             print(" Interface Name    : " + str(interface.interface))
             print(" Interface Unit    : " + str(interface.unit))
             print(" Interface Address : " + str(interface.address))
+            print(" Interface Status  : " + str(interface.status))
             print(" ")
 
 
@@ -21,11 +22,17 @@ class Interface:
         self.interface = None
         self.unit = None
         self.address = None
+        self.status = None
 
     def add_interface(self, interface, unit, address):
         self.interface = interface
         self.unit = unit
         self.address = address
+        self.status = "enabled"
+
+    def disable_interface(self, interface, unit):
+        if self.status is None:
+            self.status = "disabled"  # ANCHOR
 
 
 srx_config = SRXConfig()
@@ -50,11 +57,21 @@ def run(config):
         if line[1] == "interfaces":
             flag = line[1]
 
-        if flag == "interfaces" and len(line) > 8:
-            if line[5] == "family" and line[6] == "inet" and line[7] == "address":
-                interface = Interface()
-                # print(" > " + line[2] + " > " + line[4] + " > " + line[8])
-                interface.add_interface(line[2], line[4], line[8])
-                srx_config.append_interface(interface)
+        if flag == "interfaces":
+            if len(line) > 8:
+                if line[5] == "family" and line[6] == "inet" and line[7] == "address":
+                    interface = Interface()
+                    # print(" > " + line[2] + " > " + line[4] + " > " + line[8])
+                    interface.add_interface(line[2], line[4], line[8])
+                    srx_config.append_interface(interface)
+            if (
+                line[0] == "deactivate" and line[1] == "interfaces"
+            ):  # DELETE LINE[1] IN FUTURE
+                interface.disable_interface(line[2], line[4])
+            if (
+                line[-1] == "disable" and line[1] == "interfaces"
+            ):  # DELETE LINE[1] IN FUTURE
+                if len(line) != 4:
+                    interface.disable_interface(line[2], line[4])
 
     return srx_config
